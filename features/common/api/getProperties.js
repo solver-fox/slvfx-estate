3f63f23d6dace90277ab16494eb98bda1af85668
solver-fox <charleshardy1225@gmail.com>
2023-06-21 16:11:28 +0500

    e1

--- features/common/api/getProperties.js
+++ features/common/api/getProperties.js
@@ -1,19 +1,22 @@
-import {axios} from "@/lib/axios";
-
+import { axios } from "@/lib/axios";
 
 export const getProperties = async (num) => {
-    const {data} = await axios.get("/properties/list", {
-
-        params: {
-            locationExternalIDs: '5002,6020',
-            purpose: 'for-sale',
-            hitsPerPage: 'num',
-            lang: 'en',
-            sort: 'city-level-score',
-            hasVideo: true,
-            hasFloorPlan: true,
-            hasPanorama:true,
-          },
+  try {
+    const { data } = await axios.get("/properties/list", {
+      params: {
+        locationExternalIDs: "5002,6020",
+        purpose: "for-sale",
+        hitsPerPage: num,
+        lang: "en",
+        sort: "city-level-score",
+        hasVideo: true,
+        hasFloorPlan: true,
+        hasPanorama: true,
+      },
     });
     return data.hits;
-}
+  } catch (error) {
+    console.error("Error fetching properties:", error);
+    throw error; // Rethrow the error to be handled by the calling code
+  }
+};
