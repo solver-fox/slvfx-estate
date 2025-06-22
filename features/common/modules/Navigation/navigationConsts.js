fe07d48e6c25c50c8320fdbb7023191e8ffa3a8a
DESKTOP-PACRJ1C\Legion <charleshardy1225@gmail.com>
2025-06-22 00:20:24 -0500

    update phone

--- features/common/modules/Navigation/navigationConsts.js
+++ features/common/modules/Navigation/navigationConsts.js
@@ -3,5 +3,5 @@ import { HiPhoneArrowUpRight } from "react-icons/hi2";
 export const navigationLinks = [
     {title:'Properties', link: "/properties"},
     {title:'Contact', link: "/contact"},
-    {title:'(800) 555 555', link: "tel:(800) 555 555", icon: <HiPhoneArrowUpRight/>}
+    {title:'+1 (817) 225-1341', link: "tel:(817) 225 1341", icon: <HiPhoneArrowUpRight/>}
 ];
